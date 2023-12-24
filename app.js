22a9575251c8c1a1c3194cf6ad2e3df68e491e2b
vishal kumar <vishalkumar.engr@gmail.com>
2023-12-24 14:27:50 +0530

    Feat : Get Details from ENV

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ const {
 } = require("./middlewares/authentication");
 
 const app = express();
-const PORT = 8010;
+const PORT = 8000;
 
 mongoose
-  .connect("mongodb+srv://vishalsingh9632:<password>@cluster0.ieairrz.mongodb.net/?retryWrites=true&w=majority")
+  .connect(process.env.MONGO_URL)
   .then((e) => console.log("MongoDB Connected"));
 
 app.set("view engine", "ejs");
